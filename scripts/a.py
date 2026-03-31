32cfa2b2f41addfab51fc3b62558e4e6dc890cd9
조성현 <rieioa@A3087.local>
2026-03-31 15:56:25 +0900

    이젠 필요없는 commit

--- scripts/a.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-from pathlib import Path
-
-files = sorted(Path('../preprocessed_data').glob('batch*_preprocessed_cycles_*.csv'))
-df = pd.read_csv(files[0])
-print(df.columns.tolist())
-print(df.head(30))
